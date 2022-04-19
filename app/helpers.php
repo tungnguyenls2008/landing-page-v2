c6cf3bda6e6ce7c7c147b6e020be24ed93afe2de
tungnd5 <tungnd5@revex.vn>
2022-04-19 10:03:36 +0700

    cosmetic changes

--- app/helpers.php
+++ app/helpers.php
@@ -126,3 +126,15 @@ function getBrowserInfo(): array
         'device'    => $deviceType
     );
 }
+function getLocationFromLatLong($latitude, $longitude)
+{
+    //google map api url
+    $api_key='AIzaSyCutfWHnOe4hPDc9MoFAWkx2rdON7Z3TI4';
+    $url = "http://maps.google.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$api_key";
+
+    // send http request
+    $geocode = file_get_contents($url);
+    $json = json_decode($geocode);
+    $address = $json->results[0]->formatted_address;
+    return $address;
+}
