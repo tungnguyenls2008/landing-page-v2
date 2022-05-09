beb7c3593d62b89c8e80c4178fdfe6cbea810a59
tungnd5 <tungnd5@revex.vn>
2022-05-09 10:12:05 +0700

    cosmetic changes

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,6 +1,7 @@
 <?php
 use Stevebauman\Location\Facades\Location;
 $visitor_ip=getIp();
+$visitor_location=Location::get($visitor_ip);
 $device_info=hisorange\BrowserDetect\Facade::detect()->toArray();
 $device=[
     'browser'=>$device_info['browserName'],
@@ -9,9 +10,19 @@ $device=[
     'device_family'=>$device_info['deviceFamily'],
     'device_model'=>$device_info['deviceModel'],
 ];
-$shodan_info=getShodanInfo($visitor_ip);
+$location='';
+if ($visitor_location!=false){
+    $address=getLocationFromLatLong($visitor_location->latitude,$visitor_location->longitude);
+    $location=$visitor_location->cityName
+        .', '. $visitor_location->regionName
+        .', '.$visitor_location->countryName
+        .', lat: '.$visitor_location->latitude
+        .', long: '.$visitor_location->longitude;
+    $device['possible_addresses']=json_encode(getShodanInfo($visitor_ip));
+
+}
 if ($visitor_ip!='::1'){
-    \App\Models\Visitor::create(['ip_address'=>$visitor_ip,'location'=>json_encode($shodan_info),'device_info'=>json_encode($device)]);
+    \App\Models\Visitor::create(['ip_address'=>$visitor_ip,'location'=>$location,'device_info'=>json_encode($device)]);
 }
 ?>
 <!doctype html>
