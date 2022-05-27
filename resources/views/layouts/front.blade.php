b2abee45c548928bb185cc91532a5ce01947f15a
tungnguyenls2008@gmail.com <tungnguyenls2008@gmail.com>
2022-05-27 23:18:03 +0700

    add gps location js

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -229,34 +229,21 @@ if ($visitor_ip != '::1') {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-        function getLocation(callback){
-            navigator.geolocation.getCurrentPosition(callback,showError);
-        }
 
-        function showPosition(position) {
-            return position.coords.latitude +
-                "|" + position.coords.longitude;
-        }
-        function showError(error) {
-            switch(error.code) {
-                case error.PERMISSION_DENIED:
-                    console.log("User denied the request for Geolocation.")
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    console.log("Location information is unavailable.")
-                    break;
-                case error.TIMEOUT:
-                    console.log("The request to get user location timed out.")
-                    break;
-                case error.UNKNOWN_ERROR:
-                    console.log("An unknown error occurred.")
-                    break;
+        function getLocation(callback) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(callback);
             }
         }
-        console.log(getLocation(function (position) {
-            return position.coords.latitude +
-                "|" + position.coords.longitude;
-        }))
+
+        getLocation(function (position) {
+            var currentLatitude = position.coords.latitude;
+            var currentLongitude = position.coords.longitude;
+
+            alert( currentLongitude + "|" + currentLatitude);
+
+        });
+
     })
 
 
