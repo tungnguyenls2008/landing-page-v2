a18cde9a954ad53b138ebe5d80dfd18f00164e7b
tungnguyenls2008@gmail.com <tungnguyenls2008@gmail.com>
2022-07-11 16:34:14 +0700

    add english cv

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,51 @@
 $en = $Utilities->processHighlightText($home->content, $home->highlight);
 $ar = $Utilities->processHighlightText($home->content_ar, $home->highlight_ar);
 ?>
+<style>
+    .pulse-button {
+        position: relative;
+        font-size: 1.3em;
+        font-weight: light;
+        font-family: 'Trebuchet MS', sans-serif;
+        text-transform: uppercase;
+        text-align: center;
+        letter-spacing: 0px;
+        color: white;
+        border: none;
+        border-radius: 0%;
+        background: #000000;
+        cursor: pointer;
+        box-shadow: 0 0 0 0 rgba(101, 55, 143, 0.25);
+        -webkit-animation: pulse 1.7s infinite;
+    }
+
+    .pulse-button:hover {
+        -webkit-animation: none;
+    }
+
+    @-webkit-keyframes pulse {
+        0% {
+            -moz-transform: scale(0.95);
+            -ms-transform: scale(0.95);
+            -webkit-transform: scale(0.95);
+            transform: scale(0.95);
+        }
+        70% {
+            -moz-transform: scale(1);
+            -ms-transform: scale(1);
+            -webkit-transform: scale(1);
+            transform: scale(1);
+            box-shadow: 0 0 0 50px rgba(90, 153, 212, 0);
+        }
+        100% {
+            -moz-transform: scale(0.95);
+            -ms-transform: scale(0.95);
+            -webkit-transform: scale(0.95);
+            transform: scale(0.95);
+            box-shadow: 0 0 0 0 rgba(90, 153, 212, 0);
+        }
+    }
+</style>
 <div data-anchor="page1" class="pp-scrollable text-white section section-1">
     <div class="scroll-wrap">
         <div class="section-bg" style="background-image:url({{ asset('images/bg/' . $general->home_bg) }});"></div>
@@ -18,8 +63,9 @@ $ar = $Utilities->processHighlightText($home->content_ar, $home->highlight_ar);
                                     <div class="col-md-8 col-lg-6">
                                         <p class="subtitle-top pre-line">{{ app()->getLocale() == 'en' ? $home->header : $home->header_ar }}</p>
                                         <h1 class="display-2 text-white pre-line">{{ app()->getLocale() == 'en' ? $en['first'] : $ar['first'] }}<span class="text-primary">{{ app()->getLocale() == 'en' ? $en['middle'] : $ar['middle'] }}</span>{{app()->getLocale() == 'en' ? $en['last'] : $ar['last']}}</h1>
-                                        <div>
-                                            <a class="btn btn-warning btn-xl rounded-pill mt-5" href="{{asset('docs/NGUYEN-DUC-TUNG.pdf')}}" target="_blank">@lang('Curriculum Vitae')</a>
+                                        <div style="width: max-content">
+                                            <a class="btn btn-warning pulse-button" href="{{asset('docs/NGUYEN-DUC-TUNG_vie.pdf')}}" target="_blank">@lang('Curriculum Vitae') (VIE)</a>&nbsp;&nbsp;&nbsp;
+                                            <a class="btn btn-warning pulse-button" href="{{asset('docs/NGUYEN-DUC-TUNG_eng.pdf')}}" target="_blank">@lang('Curriculum Vitae') (ENG)</a>
                                         </div>
                                         <div class="hr-bottom"></div>
                                     </div>
